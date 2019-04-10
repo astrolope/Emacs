b067098b8a22e7dbd3d1ac773887eef340b5a5ec
Garrett Criss <garrett.criss@bestbuy.com>
2019-04-10 13:37:30 -0700

    Add whitespace. DMA-0

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-  "Configuration function for user code.
+"Configuration function for user code.
 This function is called at the very end of Spacemacs initialization after
 layers configuration.
 This is the place where most of your configurations should be done. Unless it is
@@ -19,7 +19,8 @@ you should place your code here."
   (set-selection-coding-system 'utf-8)
   (prefer-coding-system 'utf-8)
   (when (display-graphic-p)
-    (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)))
+    (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
+  )
 
 
   ;; Enable Caputre Templates
@@ -27,15 +28,15 @@ you should place your code here."
   (define-key global-map "\C-cc" 'org-capture)
   ;; Custom Caputre Templates
   (setq org-capture-templates
-        '(
-          ("p" "Project" entry (file+headline "~/org/inbox.org" "Projects")
-           "** Project: %?\n %U\n  %i\n*** Requirements  \n*** Questions \n*** Tasks  %a")
-          ("m" "Meeting" entry (file+headline "~/org/inbox.org" "Meetings")
-           "** WITH: %?\n%U\n  %i\n*** Agenda\n  * \n*** Questions \n  *  %a")
-          ("t" "Todo" entry (file+headline "~/org/inbox.org" "Tasks")
-           "* TODO %?\n  %i\n  %a")
-          ("j" "Journal" entry (file+olp+datetree "~/org/journal.org")
-           "* %?\nEntered on %U\n  %i\n  %a")))
+    '(
+      ("p" "Project" entry (file+headline "~/org/inbox.org" "Projects")
+        "** Project: %?\n %U\n  %i\n*** Requirements  \n*** Questions \n*** Tasks  %a")
+      ("m" "Meeting" entry (file+headline "~/org/inbox.org" "Meetings")
+        "** WITH: %?\n%U\n  %i\n*** Agenda\n  * \n*** Questions \n  *  %a")
+      ("t" "Todo" entry (file+headline "~/org/inbox.org" "Tasks")
+        "* TODO %?\n  %i\n  %a")
+      ("j" "Journal" entry (file+olp+datetree "~/org/journal.org")
+        "* %?\nEntered on %U\n  %i\n  %a")))
 
 
   ;; Update Agenda Buffer every X seconds.
@@ -50,40 +51,38 @@ you should place your code here."
 
   (run-with-idle-timer 3 1000 #'renewOrgBuffer)
 
-
   (setq org-todo-keywords '(
-                            (sequence "☛ TODO(t)" "|" "🔧 IN PROGRESS(i)" "|" "✔ DONE(d)")
-                            (sequence "⚑ WAITING(w)" "|")
-                            (sequence "🐛 BUG(b)" "|")
-                            (sequence "✉️  MAILED(m)" "|")
-                            (sequence "❓ QUESTION(q)" "|")
-                            (sequence "|" "✘ CANCELED(c)")
-                            ))
+    (sequence "☛ TODO(t)" "|" "🔧 IN PROGRESS(i)" "|" "✔ DONE(d)")
+    (sequence "⚑ WAITING(w)" "|")
+    (sequence "🐛 BUG(b)" "|")
+    (sequence "✉️  MAILED(m)" "|")
+    (sequence "❓ QUESTION(q)" "|")
+    (sequence "|" "✘ CANCELED(c)")
+    )
+  )
                       
-
-        
-    (org-babel-do-load-languages
-     'org-babel-load-languages
-     '(
-       (awk . t)
-       (calc .t)
-       (C . t)
-       (emacs-lisp . t)
-       (haskell . t)
-       (gnuplot . t)
-       (latex . t)
-       (ledger . t)
-       (js . t)
-       (haskell . t)
-       (perl . t)
-       (python . t)
-       ;; (gnuplot . t)
-       ;; org-babel does not currently support php.  That is really sad.
-       ;;(php . t)
-       (R . t)
-       (scheme . t)
-       (http . t)
-       (sql . t)
-       (shell . t)
-       ;;(sqlite . t)
-       ))
+  (org-babel-do-load-languages
+    'org-babel-load-languages
+    '(
+      (awk . t)
+      (calc .t)
+      (C . t)
+      (emacs-lisp . t)
+      (haskell . t)
+      (gnuplot . t)
+      (latex . t)
+      (ledger . t)
+      (js . t)
+      (haskell . t)
+      (perl . t)
+      (python . t)
+      ;; (gnuplot . t)
+      ;; org-babel does not currently support php.  That is really sad.
+      ;;(php . t)
+      (R . t)
+      (scheme . t)
+      (http . t)
+      (sql . t)
+      (shell . t)
+      ;;(sqlite . t)
+      ))
